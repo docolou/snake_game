9bd7948f03f2827bcacc9d39f0013ee143afd7be
whitekenpo <whitekenpo503@gmail.com>
2019-11-13 01:55:57 +0800

    Update of snake_game
    將snake_game 寫成可以吃到點並改變點的位置。
    新增蛇與點之圖片。

--- snake_game.py
+++ snake_game.py
@@ -12,6 +12,20 @@ import time
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
+# set clock of the game(數字越大越快)
+Gb_game_clock = 120
+
+# set the width and height of base surf
+Gb_base_surf_width = 640
+Gb_base_surf_height = 480
+
+# set the width and height of snake and point
+Gb_snake_width = 22
+Gb_snake_height = 22
+
+Gb_point_width = 22
+Gb_point_height = 22
+
 # direction of snake
 HeadRight = 0
 HeadLeft = 1
@@ -38,23 +52,39 @@ def run():
     basic_font = pygame.font.SysFont("arial", 16)   # 設定字型
     
     # 開視窗
-    base_surf = pygame.display.set_mode((640, 480))
+    base_surf = pygame.display.set_mode((Gb_base_surf_width, Gb_base_surf_height))
     
     # read image
-    snake_obj = pygame.image.load('1.gif')
+    snake_obj = pygame.image.load('snake_head.png')
+    snake_obj = pygame.transform.scale( snake_obj, (Gb_snake_width, Gb_snake_height) )   # 縮小圖片大小
+    
+    point_obj = pygame.image.load('point.jpg')
+    point_obj = pygame.transform.scale( point_obj, (Gb_point_width, Gb_point_height) )   # 縮小圖片大小
+    
 
     isRunning = True # boolean for game running or not
 
-    px, py = 10, 20 # intial the position
+    snake_px, snake_py = 10, 20 # intial the position of snake
     direction = HeadRight  # intial the direction
     
+    # set the position of point
+    point_px = random.randint(0, Gb_base_surf_width)
+    point_py = random.randint(0, Gb_base_surf_height)
+            
     pygame.key.set_repeat(1)    # 打開連續鍵盤輸入
+    
+    
+    
+    main_clock = pygame.time.Clock()
     while isRunning:
+        main_clock.tick(Gb_game_clock) # set clock of the game(數字越大越快)
+        
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:   # 使用者離開
                 isRunning = False
                 
             if event.type == pygame.KEYDOWN:    # press keyboard
+                # 使用者輸入上下左右
                 if event.key == pygame.K_LEFT:
                     direction = HeadLeft  # set the direction of snake
                 if event.key == pygame.K_RIGHT:
@@ -66,20 +96,38 @@ def run():
                     
             if event.type == pygame.MOUSEBUTTONDOWN:
                 sound_obj.play()
+
                     
+        # get the attribute of snake and point
+        snake_rect_first = snake_obj.get_rect()
+        point_rect_first = point_obj.get_rect()
         
-        # go next position
-        px, py = goNextPosition( direction, px, py )
+        snake_rect = pygame.Rect(snake_px, snake_py, snake_rect_first.width, snake_rect_first.height)
+        point_rect = pygame.Rect(point_px, point_py, point_rect_first.width, point_rect_first.height)
         
+        # 如果蛇有吃到點
+        if snake_rect.colliderect(point_rect):
+            # snake body ++
+            # change position of point
+            point_px = random.randint(0, ( Gb_base_surf_width - Gb_point_width ) )
+            point_py = random.randint(0, ( Gb_base_surf_height - Gb_point_height ) )
+        
+
+        
+        # snake go next position
+        snake_px, snake_py = goNextPosition( direction, snake_px, snake_py )
+        
+        print('===============================')    # for test
         
         #設定物件屬性
         base_surf.fill(BLACK)    # 上色，讓上一張blit的圖片消失
 
-
-        base_surf.blit(snake_obj, (px, py))    # 貼 img_obj 在base_surf 的px, py
+        base_surf.blit(snake_obj, (snake_px, snake_py))    # 貼 img_obj 在base_surf 的px, py
+        
+        base_surf.blit(point_obj, (point_px, point_py))    # 貼加命菇 在base_surf 的px, py
+        
         #更新畫面
         pygame.display.update() #更新畫面
-        time.sleep(0.01) # slow down the speed of the game
     exit()
     
     
