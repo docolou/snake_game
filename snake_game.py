5c11e5303007e1ab7a43bfe07cca2350207d1872
whitekenpo <whitekenpo503@gmail.com>
2019-11-12 11:22:56 +0800

    Update snake_game.py
    將snake_game 改寫成可以持續移動

--- snake_game.py
+++ snake_game.py
@@ -1,57 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 15:34:28 2019
+
+@author: a9302
+"""
+
 import pygame
+import random
 import time
 
-pygame.init()
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
+# direction of snake
+HeadRight = 0
+HeadLeft = 1
+HeadUp = 2
+HeadDown = 3
 
-IS_RUNNING = True
-base=pygame.display.set_mode((640,480))
-game_font=pygame.font.SysFont(pygame.font.get_default_font(),40)
+def goNextPosition( headDirection, px, py ):
+    if headDirection == HeadRight:
+        px += 1
+    elif headDirection == HeadLeft:
+        px -= 1
+    elif headDirection == HeadUp:
+        py -= 1
+    elif headDirection == HeadDown:
+        py += 1
+    return px, py        
 
-sound_obj = pygame.mixer.Sound("/Users/docoben/Desktop/python 遊戲課程/sound01.wav")
-sound_obj.play()
+def run():
+    pygame.init()   # initial pygame
 
-player_imamge1 = pygame.image.load("/Users/docoben/Desktop/python 遊戲課程/santas/1.gif")
-player_imamge2 = pygame.image.load("/Users/docoben/Desktop/python 遊戲課程/santas/2.gif")
-player_imamge3 = pygame.image.load("/Users/docoben/Desktop/python 遊戲課程/santas/3.gif")
-player_imamge4 = pygame.image.load("/Users/docoben/Desktop/python 遊戲課程/santas/4.gif")
-player_imamge5 = pygame.image.load("/Users/docoben/Desktop/python 遊戲課程/santas/5.gif")
-player_imamge6 = pygame.image.load("/Users/docoben/Desktop/python 遊戲課程/santas/6.gif")
-
-santas = [player_imamge1, player_imamge2, player_imamge3, player_imamge4, player_imamge5, player_imamge6]
-px, py = 100, 200
-
-# idx = 0
-
-# player_stretchedimage = pygame.transform.scale(player_imamge1,(40,40))
-
-
-while IS_RUNNING:
-
-    for e in pygame.event.get():
-
-        if e.type == pygame.QUIT:
-            IS_RUNNING = False
-
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_RIGHT:
-                px += 1
-            if e.key == pygame.K_LEFT:
-                px -= 1
-            if e.key == pygame.K_UP:
-                py -= 1
-            if e.key == pygame.K_DOWN:
-                py += 1  
-
+    # set sound of the game
+    sound_obj = pygame.mixer.Sound('sound01.wav')
     
-    # create window
-    base.fill((0,0,0))
-    # idx %= 6
-    base.blit(santas[1], (px,py))
-    pygame.display.update() #update 視窗
-    # idx += 1
-    # time.sleep(0.1)
+    basic_font = pygame.font.SysFont("arial", 16)   # 設定字型
     
+    # 開視窗
+    base_surf = pygame.display.set_mode((640, 480))
+    
+    # read image
+    snake_obj = pygame.image.load('1.gif')
+
+    isRunning = True # boolean for game running or not
+
+    px, py = 10, 20 # intial the position
+    direction = HeadRight  # intial the direction
+    
+    pygame.key.set_repeat(1)    # 打開連續鍵盤輸入
+    while isRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isRunning = False
+                
+            if event.type == pygame.KEYDOWN:    # press keyboard
+                if event.key == pygame.K_LEFT:
+                    direction = HeadLeft  # set the direction of snake
+                if event.key == pygame.K_RIGHT:
+                    direction = HeadRight  # set the direction of snake
+                if event.key == pygame.K_UP:
+                    direction = HeadUp  # set the direction of snake
+                if event.key == pygame.K_DOWN:
+                    direction = HeadDown  # set the direction of snake
+                    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                sound_obj.play()
+                    
+        
+        # go next position
+        px, py = goNextPosition( direction, px, py )
+        
+        
+        #設定物件屬性
+        base_surf.fill(BLACK)    # 上色，讓上一張blit的圖片消失
 
 
-exit()
+        base_surf.blit(snake_obj, (px, py))    # 貼 img_obj 在base_surf 的px, py
+        #更新畫面
+        pygame.display.update() #更新畫面
+        time.sleep(0.01) # slow down the speed of the game
+    exit()
+    
+    
+    
+run()
