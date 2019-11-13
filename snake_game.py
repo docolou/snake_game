ce9994ad3e48a21009d030df5f2a251723233546
whitekenpo <whitekenpo503@gmail.com>
2019-11-13 12:08:28 +0800

    Update snake_game.py
    新增防止反方向移動
    增加snake_position list

--- snake_game.py
+++ snake_game.py
@@ -31,7 +31,7 @@ HeadLeft = 1
 HeadUp = 2
 HeadDown = 3
 
-def goNextPosition( headDirection, snake_position ):
+def goNextPosition( headDirection, snake_position, point_px, point_py, eatPoint ):
     # get the head position
     px = snake_position[0][0]
     py = snake_position[0][1]
@@ -44,6 +44,9 @@ def goNextPosition( headDirection, snake_position ):
         py -= 1
     elif headDirection == HeadDown:
         py += 1
+        
+    if eatPoint:
+        snake_position.insert(0, [point_px, point_py])
         
     if len(snake_position) > 1 :    # 如果有body
         # 刪除最後一個 body position
@@ -58,9 +61,12 @@ def goNextPosition( headDirection, snake_position ):
 
 def showSnake(base_surf, snake_obj, snakeBody_obj, snake_position):
     base_surf.blit(snake_obj, (snake_position[0][0], snake_position[0][1]))    # 貼 snake head 在base_surf 的 snake_px, snake_py
+    for i in range ( len(snake_position) ):
+        px = snake_position[i][0]
+        py = snake_position[i][1]
+        base_surf.blit(snake_obj, (px, py))    # 貼 snake head 在base_surf 的 snake_px, snake_py
     
-    
-def testEatFood(snake_obj, point_obj, snake_position, point_px, point_py, player_point):
+def eatFood(snake_obj, point_obj, snake_position, point_px, point_py):
     # get the position of snake head
     snake_px = snake_position[0][0]
     snake_py = snake_position[0][1]
@@ -74,13 +80,9 @@ def testEatFood(snake_obj, point_obj, snake_position, point_px, point_py, player
     
     # 如果蛇有吃到點
     if snake_rect.colliderect(point_rect):
-        # snake body ++
-        player_point += 1
-        # change position of point
-        point_px = random.randint(0, ( Gb_base_surf_width - Gb_point_width ) )
-        point_py = random.randint(0, ( Gb_base_surf_height - Gb_point_height ) )
-    
-    return point_px, point_py, player_point
+        return True
+    else:
+        return False
     
 def run():
     pygame.init()   # initial pygame
@@ -106,7 +108,6 @@ def run():
 
     isRunning = True # boolean for game running or not
 
-    snake_px, snake_py = 10, 20 
     
     snake_position = [] # initialize the position of snake body
     snake_position.append([10, 20]) # initialize the position of snake head
@@ -133,22 +134,30 @@ def run():
                 
             if event.type == pygame.KEYDOWN:    # press keyboard
                 # 使用者輸入上下左右
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and direction != HeadRight:
                     direction = HeadLeft  # set the direction of snake
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT and direction != HeadLeft:
                     direction = HeadRight  # set the direction of snake
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP and direction != HeadDown:
                     direction = HeadUp  # set the direction of snake
-                if event.key == pygame.K_DOWN:
+                if event.key == pygame.K_DOWN and direction != HeadUp:
                     direction = HeadDown  # set the direction of snake
                     
             if event.type == pygame.MOUSEBUTTONDOWN:
                 sound_obj.play()
-
-        point_px, point_py, player_point = testEatFood(snake_obj, point_obj, snake_position, point_px, point_py, player_point)
+                
+        eatPoint = False
+        # 如果蛇有吃到點
+        if eatFood(snake_obj, point_obj, snake_position, point_px, point_py):
+            # snake body ++
+            player_point += 1
+            # change position of point
+            point_px = random.randint(0, ( Gb_base_surf_width - Gb_point_width ) )
+            point_py = random.randint(0, ( Gb_base_surf_height - Gb_point_height ) )
+            eatPoint = True
         
         # snake go next position
-        snake_position = goNextPosition( direction, snake_position )
+        snake_position = goNextPosition( direction, snake_position, point_px, point_py, eatPoint )
         print('after next position : ', snake_position)
         
         #設定物件屬性
