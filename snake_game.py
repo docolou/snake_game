12e16082e5a18bfca8eec686a4b460de44d84e74
docolou <docolou@gmail.com>
2019-11-13 11:29:21 +0800

    新增貪吃蛇區域圖片

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 import time
 
 pygame.init()
@@ -11,53 +11,70 @@ game_font=pygame.font.SysFont(pygame.font.get_default_font(),40)
 sound_obj = pygame.mixer.Sound("/Users/docoben/Desktop/python 遊戲課程/sound01.wav")
 sound_obj.play()
 
-player_imamge1 = pygame.image.load("snake_whole.png")
+snake_head = pygame.image.load("snake_head.png")
+snake_straight = pygame.image.load("snake_straight.png")
+snake_straight = pygame.transform.scale(snake_straight,(30,30)) #縮小或放大圖片用
+snake_tail = pygame.image.load("snake_tail.png")
 
+
+
+# snake = pygame.transform.scale(snake,(80,80)) #縮小或放大圖片用
 
 px, py = 100, 200
 direction_mode = 1
 
-# idx = 0
 
-# player_stretchedimage = pygame.transform.scale(player_imamge1,(40,40))
+
 
 
 while IS_RUNNING:
 
-    for e in pygame.event.get():
+    for detect_key in pygame.event.get():
 
-        if e.type == pygame.QUIT:
+        if detect_key.type == pygame.QUIT:
             IS_RUNNING = False
 
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_RIGHT:
+        if detect_key.type == pygame.KEYDOWN:
+            if detect_key.key == pygame.K_RIGHT:
                 direction_mode = 1
-            if e.key == pygame.K_LEFT:
+            if detect_key.key == pygame.K_LEFT:
                 direction_mode = 2
-            if e.key == pygame.K_UP:
+            if detect_key.key == pygame.K_UP:
                 direction_mode = 3
-            if e.key == pygame.K_DOWN:
+            if detect_key.key == pygame.K_DOWN:
                 direction_mode = 4
 
-        
-    if direction_mode == 1:
-        px += 10  
-    elif direction_mode == 2:
-        px -= 10  
-    elif direction_mode == 3:
-        py -= 10 
-    elif direction_mode == 4:
-        py += 10
-    
 
-    time.sleep(1)
+    point = [(100, 200), (130, 200), (160, 200), (160, 170)]
+    point = { }
+
+    point_px = [100, 130, 160]
+    point_py = [200, 200, 200]
+
+    if direction_mode == 1:
+        px += 30 
+    elif direction_mode == 2:
+        px -= 30  
+    elif direction_mode == 3:
+        py -= 30 
+    elif direction_mode == 4:
+        py += 30
+
+  
+    
+    
     # create window
-    base.fill((0,0,0))
-    # idx %= 6
-    base.blit(player_imamge1, (px,py))
+    base.fill((0,0,0)) #避免殘影
+
+    # base.blit(snake_head, (px,py-300))
+    # base.blit(snake_straight, (px,py))    
+    # base.blit(snake_straight, (px+30,py))
+    # base.blit(snake_straight, (px+60,py))
+    for point_px,point_py in point:
+        base.blit(snake_straight, (point_px,point_py))    
+    # base.blit(snake_tail, (px,py+67))
     pygame.display.update() #update 視窗
-    # idx += 1
-    # time.sleep(0.1)
+    time.sleep(0.5)
     
 
 
